f9eef351c66810a9917f1cd9c18bfd31ef53fcc9
Harmen Prins <prins_harmen@hotmail.com>
2020-09-09 20:10:30 +0200

    Add Interpreter class
    Takes care of applying interpreter functions and aggregators over columns, chunks and boxes.

--- stock-filters/open_debugger.py
+++ stock-filters/open_debugger.py
@@ -6,31 +6,14 @@ from mcplatform import *
 
 import numpy as np
 
-from pymclevel import MCLevel, BoundingBox, MCInfdevOldLevel
+from pymclevel import MCLevel, BoundingBox, MCInfdevOldLevel, materials
+
+from village_generation.interpreter.interpreter import LevelColumnInterpreter, Interpreter
 
 inputs = (
 	("Open Debugger", "label"),
 	("Creator: Pluralis", "label"),
-	)
-
-
-def calculate_height_map(level, box):
-	chunk_box = box.chunkBox(level)
-	height_map = np.zeros((chunk_box.width, chunk_box.length))
-	for chunk_coord in chunk_box.chunkPositions:
-		map_coord = (chunk_coord[0] - chunk_box.mincx) << 4, (chunk_coord[1] - chunk_box.mincz) << 4
-		chunk_tensor = level.getChunk(*chunk_coord).Blocks  # type:np.ndarray
-		air_tensor = (chunk_tensor == level.materials.Air.ID)
-		# argmin grabs the first non-air (False) it encounters.
-		# Since we want to search from the TOP, we first flip the tensor in the y dimension
-		height_map[map_coord[0]:map_coord[0]+16, map_coord[1]:map_coord[1]+16] = 255 - air_tensor[:,:,::-1].argmin(2)
-
-	# extract box from chunk_box
-	m_left = box.minx - chunk_box.minx
-	m_right = chunk_box.maxx - box.maxx
-	m_top = box.minz - chunk_box.minz
-	m_bot = chunk_box.maxz - box.maxz
-	return height_map[m_left:height_map.shape[0]-m_right, m_top:height_map.shape[1]-m_bot]
+)
 
 
 def calculate_relief_map(height_map, r=16):
@@ -45,16 +28,16 @@ def calculate_relief_map(height_map, r=16):
 	"""
 	# TODO: Rewrite to iterate over chunks, not r
 	width, length = height_map.shape
-	relief_map = np.zeros((int(ceil(width/float(r))), int(ceil(length/float(r)))))
+	relief_map = np.zeros((int(ceil(width / float(r))), int(ceil(length / float(r)))))
 	for x in range(0, width, r):
 		for y in range(0, length, r):
-			chunk = height_map[x:x+r, y:y+r]  # type:np.ndarray
-			relief_map[int(x / r), int(y / r)] = chunk.var()
+			chunk = height_map[x:x + r, y:y + r]  # type:np.ndarray
+			relief_map[int(x / r), int(y / r)] = chunk.std()
 	return relief_map
 
 
 def aggregate_height_per_chunk(height_map):
-	r=16
+	r = 16
 	width, length = height_map.shape
 	agg_height_map = np.zeros((int(ceil(width / float(r))), int(ceil(length / float(r)))))
 	for x in range(0, width, r):
@@ -62,6 +45,17 @@ def aggregate_height_per_chunk(height_map):
 			chunk = height_map[x:x + r, y:y + r]  # type:np.ndarray
 			agg_height_map[int(x / r), int(y / r)] = chunk.mean()
 	return agg_height_map
+
+
+class HeightInterpreter(Interpreter):
+
+	def interpret(self, obj):
+		return 255 - (obj == materials.classicMaterials.Air.ID)[::-1].argmin()
+
+
+def center_level(level, height_map):
+	"""Shift columns in level such that the surface is flat"""
+	MCInfdevOldLevel
 
 
 def perform(level, box, options):
@@ -72,13 +66,157 @@ def perform(level, box, options):
 		box (BoundingBox): Box that limits where the village can be placed
 		options (dict): Options given to the Filter from MCEdit2
 	"""
-	import ipdb
-	# ipdb.set_trace()
 	print("TIME TO DEBUG")
 
-	height_map = calculate_height_map(level, box)  # type: np.ndarray
+	height_mapper = LevelColumnInterpreter(HeightInterpreter())
+	height_map = height_mapper.interpret((level, box))
+	centered_level = center_level(level, height_map)
 	relief_map = calculate_relief_map(height_map)  # type: np.ndarray
 	agg_height_map = aggregate_height_per_chunk(height_map)  # type: np.ndarray
+	# set_height_with_bricks(agg_height_map, box, level)
+	build_coords = find_build_area(relief_map, height_map, level, box, n=100)
+	build_map = np.zeros_like(relief_map, dtype=bool)
+	build_map[build_coords] = True
+	set_height_with_bricks(100 * build_map, box, level)
+
+
+def _out_of_bounds(coord, area_shape):
+	return coord[0] < 0 \
+		   or coord[0] >= area_shape[0] \
+		   or coord[1] < 0 \
+		   or coord[1] >= area_shape[1]
+
+
+def _calculate_relief_scores(relief_map):
+	area_map = np.zeros_like(relief_map, dtype=int)
+	cutoffs = np.sort(relief_map.flatten())
+
+	base_factor = 0.9
+	factor = 1
+	for score in range(1, 21):
+		factor *= base_factor
+		cutoff = cutoffs[int(len(cutoffs) * factor)]
+		area_map[relief_map <= cutoff] = score
+	return area_map
+
+
+def overlay_on_map(
+		grid  # type: np.ndarray
+):
+	image_size = 792., 792.
+	square_size = (image_size[0] / grid.shape[0],
+				   image_size[1] / grid.shape[1])
+
+	import matplotlib.pyplot as plt
+	import scipy.sparse as sps
+	img = plt.imread("SecondWorldMap.png")
+	fig, ax = plt.subplots()
+	ax.imshow(img)
+
+	coordinate_grid = sps.coo_matrix(grid)
+	x = coordinate_grid.col * square_size[0] + square_size[0] / 2
+	y = coordinate_grid.row * square_size[1] + square_size[1] / 2
+	v = coordinate_grid.data
+	plt.scatter(x, y, c=v, s=100, marker="+")
+	plt.show()
+
+
+def _build_material_map(level, box, height_map, materials, search_depth, blocks_needed=1):
+	water_map = np.zeros((box.maxcx - box.mincx, box.maxcz - box.mincz), dtype=int)
+
+	for chunk_coord in box.chunkPositions:
+		map_coord = (chunk_coord[1] - box.mincx) << 4, (chunk_coord[0] - box.mincz) << 4
+		relevant_height_map = height_map[map_coord[1]:map_coord[1] + 16, map_coord[0]:map_coord[0] + 16]
+		chunk_tensor = level.getChunk(*chunk_coord).Blocks  # type:np.ndarray
+		chunk_surface_tensor = chunk_tensor[:, :,
+							   int(relevant_height_map.min()) - search_depth:int(relevant_height_map.max())]
+		chunk_surface_tensor = np.isin(chunk_surface_tensor, [m.ID for m in materials])
+		water_map[chunk_coord[1] - box.mincz, chunk_coord[0] - box.mincx] = chunk_surface_tensor.sum() >= blocks_needed
+	return water_map
+
+
+def _spread_scores_over_map(
+		score_map,  # type: np.ndarray
+		subtract=True
+):
+	neighbour_scores = np.zeros(score_map.shape + (4,))
+	neighbour_scores[1:, :, 0] = score_map[:-1, :]
+	neighbour_scores[:-1, :, 1] = score_map[1:, :]
+	neighbour_scores[:, 1:, 2] = score_map[:, :-1]
+	neighbour_scores[:, :-1, 3] = score_map[:, 1:]
+	max_neighbour_score = neighbour_scores.max(2) - subtract
+	return np.maximum(score_map, max_neighbour_score)
+
+
+def _calculate_water_scores(level, box, height_map):
+	water = [level.materials.Water, level.materials.WaterActive]
+	# TODO: deal with chunk boxing in perform
+	chunk_box = box.chunkBox(level)
+	score_map = _build_material_map(level, chunk_box, height_map, water, 5, 10) * 10
+	for i in range(10):
+		score_map = _spread_scores_over_map(score_map)
+	return score_map
+
+
+def _calculate_tree_scores(level, box, height_map):
+	chunk_box = box.chunkBox(level)
+	score_map = _build_material_map(level, chunk_box, height_map, [level.materials.Wood], 20, 10) * 10
+	for i in range(10):
+		score_map = _spread_scores_over_map(score_map)
+	return score_map
+
+
+def _calculate_rock_scores(level, box, height_map):
+	chunk_box = box.chunkBox(level)
+	# TODO: Stone are 3 bricks below dirt, so if there is any relief, there will be Stone in a chunk
+	#    So we need another way to define the surface for Stone
+	score_map = _build_material_map(level, chunk_box, height_map, [level.materials.Stone], -5, 100) * 10
+	for i in range(13):
+		score_map = _spread_scores_over_map(score_map, subtract=i < 3)
+	return score_map
+
+
+def _calculate_resource_scores(level, box, height_map):
+	tree_scores = _calculate_tree_scores(level, box, height_map)
+	return tree_scores
+
+
+# rock_scores = _calculate_rock_scores(level, box, height_map)
+# return np.maximum(tree_scores, rock_scores) + np.minimum(tree_scores, rock_scores)/2
+
+
+def find_build_area(
+		relief_map,  # type: np.ndarray
+		height_map,
+		level,
+		box,
+		n=None
+):
+	"""Find all areas where the first buildings will be made
+
+	The algorithm attributes scores to every chunk
+		- 0 - 20 for relief
+		- 0 - 15 for proximity to water
+		- 0 - 15 for proximity to resources
+			- 0 - 10 for proximity to trees
+			- 0 - 10 for proximity to rocks
+			score is 100% of highest + 50% of lowest
+
+	If n is None (default), return scores for all chunks in a grid shape
+	If n is an integer, return a 2xn tuple with coordinates of the top n chunks (order not guaranteed)
+	"""
+	relief_scores = _calculate_relief_scores(relief_map)
+	water_scores = _calculate_water_scores(level, box, height_map)
+	resource_scores = _calculate_resource_scores(level, box, height_map)
+
+	scores = relief_scores + water_scores + resource_scores
+	scores = np.argsort(scores, axis=None)
+	if n is not None:
+		return np.unravel_index(scores[:n], relief_scores.shape)
+	return scores
+
+
+def set_height_with_bricks(agg_height_map, box, level):
 	for xc, zc in itertools.product(xrange(agg_height_map.shape[0]), xrange(agg_height_map.shape[1])):
 		y = int(agg_height_map[xc, zc])
 		xc, zc = (xc << 4) + box.minx, (zc << 4) + box.minz
---
+++ stock-filters/village_generation/interpreter/interpreter.py
@@ -0,0 +1,110 @@
+from abc import ABCMeta, abstractmethod
+
+import numpy as np
+
+
+CHUNK_WIDTH = CHUNK_HEIGHT = 16
+
+
+class Interpreter:
+	__metaclass__ = ABCMeta
+
+	@abstractmethod
+	def interpret(self, obj):
+		"""Transform the given object into a grid, classifying each subjection.
+
+		Args:
+			obj: The object to be interpreted
+
+		Returns: A np.ndarray corresponding to the object with an additional optional dimension for multiple variables
+		of interpretation
+
+		"""
+		pass
+
+
+class ColumnInterpreter(Interpreter):
+	__metaclass__ = ABCMeta
+
+
+class ChunkInterpreter(Interpreter):
+	def __init__(self, column_interpreter, aggregator, output_dimension=(1,)):
+		"""
+
+		Args:
+			column_interpreter (Interpreter): Is applied to every column in the chunk
+			aggregator (Callable): Aggregates the output of the column_interpreter calls
+			output_dimension (tuple): Size of aggregator output
+		"""
+		self.__column_interpreter = column_interpreter
+		self.__aggregator = aggregator
+		self.output_dimension = output_dimension
+
+	def interpret(self, obj):
+		"""Interpret a Chunk to a single object
+
+		Args:
+			chunk (np.ndarray): 16x16x? ndarray with Block IDs
+
+		Returns: Output of aggregator
+		"""
+		chunk = obj
+		column_interpretations = []
+		for x in xrange(CHUNK_WIDTH):
+			partial_column_interpretations = []
+			for z in xrange(CHUNK_HEIGHT):
+				column = chunk[x, z, :]
+				column_interpretation = self.__column_interpreter.interpret(column)
+				partial_column_interpretations.append(column_interpretation)
+			column_interpretations.append(partial_column_interpretations)
+		return self.__aggregator(column_interpretations)
+
+
+class LevelChunkInterpreter(Interpreter):
+	def __init__(
+			self,
+			chunk_interpreter,  # type: ChunkInterpreter
+	):
+		self.chunk_interpreter = chunk_interpreter
+
+	def interpret(self, obj):
+		level, box = obj
+		output_shape = self._calculate_output_shape(box)
+		output = np.empty(output_shape + self.chunk_interpreter.output_dimension)
+		for chunk_position in box.chunkPositions:
+			output_position = self._to_output_position(chunk_position, box)
+			chunk = level.getChunk(*chunk_position)
+			chunk_interpretation = self.chunk_interpreter.interpret(chunk.Blocks)
+			output[output_position[0], output_position[1], :] = chunk_interpretation
+
+		return output
+
+	@staticmethod
+	def _calculate_output_shape(box):
+		return box.maxcx - box.mincx, box.maxcz - box.mincz
+
+	@staticmethod
+	def _to_output_position(chunk_position, box):
+		return chunk_position[0] - box.mincx, chunk_position[1] - box.mincz
+
+
+class LevelColumnInterpreter(LevelChunkInterpreter):
+	# TODO: support multi-dimensional column interpreter output: (16,16,x)
+	def __init__(self, column_interpreter):
+		super(LevelColumnInterpreter, self).__init__(ChunkInterpreter(column_interpreter, np.asarray, (16, 16)))
+
+	def interpret(self, obj):
+		level, box = obj
+		interpretation_4d = super(LevelColumnInterpreter, self).interpret(obj)
+		shape = (box.size.x, box.size.z)
+		if box.size.x * box.size.z != interpretation_4d.size:
+			shape = shape + (-1,)
+		return np.reshape(interpretation_4d, shape)
+
+# TODO: most interpreters work near the surface. To make it easier to handle, let's make a Level where the surface is
+#  at the same level everywhere: shift every column with its heightmap offset from the mean. Fill with bedrock/air.
+#  Then we can take a e.g. 10-height slice (5 above, 5 below surface) and interpret that. That way, we do not have to
+#  pass the heightmap to every column interpreter.
+#  We still need to be able to pass full maps to a LevelInterpreter, which are then sliced and the slice handed to
+#  the chunk/column interpreter (so it matches with the level slice it is interpreting)
+
